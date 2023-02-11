e004dfff63619fe0b61f8322efd5db144b344511
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-11 12:24:20 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1742,7 +1742,27 @@ class PlayState extends MusicBeatState
 			startCountdown();
 	}
 
+	var tb_x = 60;
+	var tb_y = 410;
+	var tb_fx = -510 + 40;
+	var tb_fy = 320;
+	var tb_rx = 200 - 55;
+	var jx:Int;
+
+	var curr_char:Int;
+	var curr_dial:Int;
+	var dropText:FlxText;
+	var tbox:FlxSprite;
+	var talk:Int;
+	var tb_appear:Int;
+	var dcd:Int;
+	var fimage:String;
+	var fsprite:FlxSprite;
+	var fside:Int;
+	var black:FlxSprite;
+	var tb_open:Bool = false;
 	var dialogueCount:Int = 0;
+
 	public var psychDialogue:DialogueBoxPsych;
 	//You don't have to add a song, just saying. You can just do "startDialogue(dialogueJson);" and it should work
 	public function startDialogue(dialogueFile:DialogueFile, ?song:String = null):Void
