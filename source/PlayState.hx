0310badda7087e23a1660526613732654b749368
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-02-11 10:09:58 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -164,6 +164,26 @@ class PlayState extends MusicBeatState
     //more keys
 	public static var mania = 0;
 
+	//shaggg
+	var rock:FlxSprite;
+	var gf_rock:FlxSprite;
+	var doorFrame:FlxSprite;
+	var legs:FlxSprite;
+	var shaggyT:FlxTrail;
+	var legT:FlxTrail;
+	var burst:FlxSprite;
+
+	//cum
+	var camLerp:Float = 1;
+	var bgDim:FlxSprite;
+	var fullDim = false;
+	var noticeTime = 0;
+	var dimGo:Bool = false;
+
+	//cutscenxs
+	var cutTime = 0;
+	var sEnding = 'none';
+
 	//Handles the new epic mega sexy cam code that i've done
 	public var camFollow:FlxPoint;
 	public var camFollowPos:FlxObject;
@@ -459,6 +479,14 @@ class PlayState extends MusicBeatState
 					curStage = 'schoolEvil';
 				case 'ugh' | 'guns' | 'stress':
 					curStage = 'tank';
+			        case 'where-are-you' | 'eruption' | 'kaio-ken' | 'whats-new' | 'blast' | 'super-saiyan' | 'overflow':
+					curStage = 'mansion';
+			        case 'god-eater':
+					curStage = 'sky';
+				case 'soothing-power' | 'thunderstorm' | 'dissasembler':
+	         			curStage = 'out';
+				case 'astral-calamity' | 'talladega' | 'big-shot':
+					curStage = 'lava';
 				default:
 					curStage = 'stage';
 			}
@@ -537,9 +565,143 @@ class PlayState extends MusicBeatState
 					stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
 					stageCurtains.updateHitbox();
 					add(stageCurtains);
-				}
-				dadbattleSmokes = new FlxSpriteGroup(); //troll'd
+			    }
+			case 'mansion':
+				var bg:FlxSprite = new FlxSprite(-400, -160).loadGraphic(Paths.image('bg_lemon'));
+				bg.setGraphicSize(Std.int(bg.width * 1.5));
+				bg.antialiasing = true;
+				bg.scrollFactor.set(0.95, 0.95);
+				bg.active = false;
+				add(bg);
+			
+			case 'sky':
+				var sky = new FlxSprite(-850, -850);
+				sky.frames = Paths.getSparrowAtlas('god_bg');
+				sky.animation.addByPrefix('sky', "bg", 30);
+				sky.setGraphicSize(Std.int(sky.width * 0.8));
+				sky.animation.play('sky');
+				sky.scrollFactor.set(0.1, 0.1);
+				sky.antialiasing = true;
+				add(sky);
 
+				var bgcloud = new FlxSprite(-850, -1250);
+				bgcloud.frames = Paths.getSparrowAtlas('god_bg');
+				bgcloud.animation.addByPrefix('c', "cloud_smol", 30);
+				//bgcloud.setGraphicSize(Std.int(bgcloud.width * 0.8));
+				bgcloud.animation.play('c');
+				bgcloud.scrollFactor.set(0.3, 0.3);
+				bgcloud.antialiasing = true;
+				add(bgcloud);
+
+				add(new MansionDebris(300, -800, 'norm', 0.4, 1, 0, 1));
+				add(new MansionDebris(600, -300, 'tiny', 0.4, 1.5, 0, 1));
+				add(new MansionDebris(-150, -400, 'spike', 0.4, 1.1, 0, 1));
+				add(new MansionDebris(-750, -850, 'small', 0.4, 1.5, 0, 1));
+
+				/*
+				add(new MansionDebris(-300, -1700, 'norm', 0.5, 1, 0, 1));
+				add(new MansionDebris(-600, -1100, 'tiny', 0.5, 1.5, 0, 1));
+				add(new MansionDebris(900, -1850, 'spike', 0.5, 1.2, 0, 1));
+				add(new MansionDebris(1500, -1300, 'small', 0.5, 1.5, 0, 1));
+				*/
+
+				add(new MansionDebris(-300, -1700, 'norm', 0.75, 1, 0, 1));
+				add(new MansionDebris(-1000, -1750, 'rect', 0.75, 2, 0, 1));
+				add(new MansionDebris(-600, -1100, 'tiny', 0.75, 1.5, 0, 1));
+				add(new MansionDebris(900, -1850, 'spike', 0.75, 1.2, 0, 1));
+				add(new MansionDebris(1500, -1300, 'small', 0.75, 1.5, 0, 1));
+				add(new MansionDebris(-600, -800, 'spike', 0.75, 1.3, 0, 1));
+				add(new MansionDebris(-1000, -900, 'small', 0.75, 1.7, 0, 1));
+
+				var fgcloud = new FlxSprite(-1150, -2900);
+				fgcloud.frames = Paths.getSparrowAtlas('god_bg');
+				fgcloud.animation.addByPrefix('c', "cloud_big", 30);
+				//bgcloud.setGraphicSize(Std.int(bgcloud.width * 0.8));
+				fgcloud.animation.play('c');
+				fgcloud.scrollFactor.set(0.9, 0.9);
+				fgcloud.antialiasing = true;
+				add(fgcloud);
+
+				var bg:FlxSprite = new FlxSprite(-400, -160).loadGraphic(Paths.image('bg_lemon'));
+				bg.setGraphicSize(Std.int(bg.width * 1.5));
+				bg.antialiasing = true;
+				bg.scrollFactor.set(0.95, 0.95);
+				bg.active = false;
+				add(bg);
+
+				var techo = new FlxSprite(0, -20);
+				techo.frames = Paths.getSparrowAtlas('god_bg');
+				techo.animation.addByPrefix('r', "broken_techo", 30);
+				techo.setGraphicSize(Std.int(techo.frameWidth * 1.5));
+				techo.animation.play('r');
+				techo.scrollFactor.set(0.95, 0.95);
+				techo.antialiasing = true;
+				add(techo);
+
+				gf_rock = new FlxSprite(20, 20);
+				gf_rock.frames = Paths.getSparrowAtlas('god_bg');
+				gf_rock.animation.addByPrefix('rock', "gf_rock", 30);
+				gf_rock.animation.play('rock');
+				gf_rock.scrollFactor.set(0.8, 0.8);
+				gf_rock.antialiasing = true;
+				add(gf_rock);
+
+				rock = new FlxSprite(20, 20);
+				rock.frames = Paths.getSparrowAtlas('god_bg');
+				rock.animation.addByPrefix('rock', "rock", 30);
+				rock.animation.play('rock');
+				rock.scrollFactor.set(1, 1);
+				rock.antialiasing = true;
+				add(rock);
+
+				//god eater legs
+				legs = new FlxSprite(-850, -850);
+				legs.frames = Paths.getSparrowAtlas('characters/pshaggy');
+				legs.animation.addByPrefix('legs', "solo_legs", 30);
+				legs.animation.play('legs');
+				legs.antialiasing = true;
+				legs.updateHitbox();
+				legs.offset.set(legs.frameWidth / 2, 10);
+				legs.alpha = 0;
+				}
+
+			case 'out':
+
+				var sky:BGElement = new BGElement('OBG/sky', -1204, -456, 0.15, 1, 0);
+				add(sky);
+
+				var clouds:BGElement = new BGElement('OBG/clouds', -988, -260, 0.25, 1, 1);
+				add(clouds);
+
+				var backMount:BGElement = new BGElement('OBG/backmount', -700, -40, 0.4, 1, 2);
+				add(backMount);
+
+				var middleMount:BGElement = new BGElement('OBG/middlemount', -240, 200, 0.6, 1, 3);
+				add(middleMount);
+
+				var ground:BGElement = new BGElement('OBG/ground', -660, 624, 1, 1, 4);
+				add(ground);
+
+			case 'lava':
+				if (SONG.song != 'Astral-calamity') defaultCamZoom = 0.6;
+
+				curStage = 'lava';
+
+				var bgbg:BGElement = new BGElement('WBG/BGBG', -1940, -1112, 0.5, 1, 0);
+				add(bgbg);
+
+				var lavaLimits:BGElement = new BGElement('WBG/LavaLimits', -1770, 168, 0.55, 1, 1);
+				add(lavaLimits);
+
+				var bgSpikes:BGElement = new BGElement('WBG/BGSpikes', 112, -36, 0.6, 1, 2);
+				add(bgSpikes);
+
+				var spikes:BGElement = new BGElement('WBG/Spikes', -1186, -234, 0.8, 1, 3);
+				add(spikes);
+
+				var ground:BGElement = new BGElement('WBG/Ground', -1320, 590, 1, 1, 4);
+				add(ground);
+		
 			case 'spooky': //Week 2
 				if(!ClientPrefs.lowQuality) {
 					halloweenBG = new BGSprite('halloween_bg', -200, -100, ['halloweem bg0', 'halloweem bg lightning strike']);
